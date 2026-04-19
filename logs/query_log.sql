2fa68ab88cbff3b675ae5d5952f8c7304a160f90
Avinash-VN <AvinashV@buffalo.edu>
2026-04-19 00:43:46 -0400

    test

--- logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2026-04-19T04:31:17.782998800+00:00
--- finished_at: 2026-04-19T04:31:17.928523500+00:00
--- elapsed: 145ms
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01c3ce4f-3203-edda-0007-903e0005f0b6
--- desc: not provided
-select 1 as id;
